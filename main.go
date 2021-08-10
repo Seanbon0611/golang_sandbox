50a5ca690394dae68b09d9fad34d0b10a38af936
Seanbon0611 <sean.dever@datadoghq.com>
2021-08-09 22:15:41 -0700

    added Dog model, Get HTTP request and well as POST HTTP request to add a new dog

--- main.go
+++ main.go
@@ -1,25 +1,62 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
+	"math/rand"
 	"net/http"
+	"strconv"
 
 	muxtrace "gopkg.in/DataDog/dd-trace-go.v1/contrib/gorilla/mux"
+	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
 )
 
-type User struct {
-	ID        string `json:"id"`
-	Firstname string `json:"firstname"`
-	Lastname  string `json:"lastname"`
+type Dog struct {
+	ID    string `json:"id"`
+	Breed string `json:"breed"`
+	Name  string `json:"name"`
+	Age   string `json:"age"`
+	Notes string `json:"notes"`
 }
 
-func HomeHandler(w http.ResponseWriter, r *http.Request) {
+func getDogs(w http.ResponseWriter, r *http.Request) {
+	span := tracer.StartSpan("web.request", tracer.ResourceName("/dogs"))
 
+	defer span.Finish()
+	// Set tag
+	span.SetTag("http.url", r.URL.Path)
+	span.SetTag("testkey", "testvalue")
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(dogs)
 }
+
+func addDog(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var dog Dog
+	_ = json.NewDecoder(r.Body).Decode(&dog)
+	dog.ID = strconv.Itoa(rand.Intn(10000000))
+	dogs = append(dogs, dog)
+	json.NewEncoder(w).Encode(dog)
+}
+
+var dogs []Dog
 
 func main() {
-	fmt.Println("Hello, world")
+	tracer.Start(
+		tracer.WithService("test1"),
+		tracer.WithEnv("prod"),
+	)
+	defer tracer.Stop()
 	mux := muxtrace.NewRouter()
-	mux.HandleFunc("/", HomeHandler)
 
+	dogs = append(dogs, Dog{ID: "1", Breed: "Shiba Inu", Name: "Hachiko", Age: "10", Notes: "A bit of a brat"})
+	dogs = append(dogs, Dog{ID: "2", Breed: "Golden Retriever", Name: "Lassy", Age: "8", Notes: "Likes to run"})
+	mux.HandleFunc("/dogs", getDogs).Methods("GET")
+	mux.HandleFunc("/dogs/new", addDog).Methods("POST")
+	// mux.HandleFunc("/dogs/{id}", getSingleDog).Methods(("GET"))
+	port := ":8080"
+	fmt.Println("\nListening on port " + port)
+	log.Fatal(http.ListenAndServe(port, mux))
 }
